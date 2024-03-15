8b8a1f57ce707c7b0ca6feae838702eb0eceffc8
Maksim Mamontov <maksimamontov@gmail.com>
2024-03-15 19:11:12 +0200

    First Commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.ir.backend.js.compile
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+}
+
+kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(project.projectDir.path)
+                    }
+                }
+            }
+        }
+    }
+    
+    jvm()
+    
+    sourceSets {
+        commonMain.dependencies {
+            // put your Multiplatform dependencies here
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
